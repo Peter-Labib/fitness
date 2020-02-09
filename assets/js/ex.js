44c261eaff902f09137f25a4afd08b087afaf395
Peter-Labib <peter.adel.labib@gmail.com>
2020-02-09 06:54:46 +0200

    get 1st version of our trainer section

---
+++ assets/js/ex.js
@@ -0,0 +1,5 @@
+const carouselItem = document.querySelectorAll('.our-trainer__pic .owl-item');
+const carouselActiveItem = document.querySelector('.our-trainer__pic .owl-item.center').children;
+const ourTrainerInfo = document.querySelectorAll('.our-trainer__info');
+console.log(carouselItem);
+console.log(carouselActiveItem);
