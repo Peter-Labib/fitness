63b907ad5179a304b3bcacf0fc195bedaadb0141
Peter-Labib <peter.adel.labib@gmail.com>
2020-02-11 06:17:37 +0200

    finish Recent Blog section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,32 @@
-// eslint-disable-next-line no-undef
-$('.owl-carousel').owlCarousel({
+/* eslint-disable no-undef */
+$('.owl-carousel.our-trainer__pic').owlCarousel({
 	center: true,
-	items:2,
-	loop:true,
-	nav:true,
-	navText: ['<i class="fas fa-chevron-circle-left fa-2x"></i>','<i class="fas fa-chevron-circle-right fa-2x"></i>'],
-	margin:10,
-	responsive:{
-		600:{
-			items:3
+	items: 2,
+	loop: true,
+	nav: true,
+	navText: ['<i class="fas fa-chevron-circle-left fa-2x"></i>', '<i class="fas fa-chevron-circle-right fa-2x"></i>'],
+	margin: 10,
+	responsive: {
+		600: {
+			items: 3
+		}
+	}
+});
+
+$('.Testmonials__carousel.owl-carousel').owlCarousel({
+	center: true,
+	items: 1,
+	loop: true,
+	animateOut: 'fadeOut',
+	autoplayTimeout: 10000,
+	autoplay:true,
+	autoplayHoverPause: true,
+	nav: true,
+	navText: ['<i class="fas fa-chevron-circle-left"></i>', '<i class="fas fa-chevron-circle-right"></i>'],
+	margin: 10,
+	responsive: {
+		600: {
+			items: 1
 		}
 	}
 });
