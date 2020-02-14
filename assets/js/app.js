7221280e84a05165e5fef4244eb31dfc253b9de9
Peter-Labib <peter.adel.labib@gmail.com>
2020-02-14 22:52:31 +0200

    project finished

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,9 +13,23 @@ $('.owl-carousel.our-trainer__pic').owlCarousel({
 	}
 });
 
+$('.owl-carousel.our-trainer__second-carousel').owlCarousel({
+	center: true,
+	// items: 2,
+	loop: true,
+	nav: true,
+	navText: ['<i class="fas fa-chevron-circle-left fa-2x"></i>', '<i class="fas fa-chevron-circle-right fa-2x"></i>'],
+	// margin: 10,
+	responsive: {
+		600: {
+			items: 5
+		}
+	}
+});
+
 $('.Testmonials__carousel.owl-carousel').owlCarousel({
 	center: true,
-	items: 1,
+	// items: 1,
 	loop: true,
 	animateOut: 'fadeOut',
 	autoplayTimeout: 10000,
